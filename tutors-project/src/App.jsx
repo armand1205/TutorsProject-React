5535300fcb84f6dd42030e99b18785e13aee95b4
armand1205 <munteanuarmandshadow1205@gmail.com>
2025-02-11 01:49:52 +0200

    day 1.2

--- tutors-project/src/App.jsx
+++ tutors-project/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./components/Sidebar/Sidebar";
 import Main from "./components/Main";
 
 // import Menu from "./components/Menu";
